c8b13fcf3225bd6279dde9d9e711e328cce6b3e5
srivatsanrp <40450617+srivatsanrp@users.noreply.github.com>
2019-11-01 19:21:21 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,17 @@
+import json
+from pykafka import KafkaClient
+from pykafka.exceptions import SocketDisconnectedError
+from pykafka.exceptions import LeaderNotAvailable 
+from websocket import create_connection
+
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics['test']
+producer = topic.get_producer()
+topic.get_sync_producer() 
+ws = create_connection("wss://ws.blockchain.info/inv")
+ws.send(json.dumps({"op": "unconfirmed_sub"}))
+while True:
+    result = json.loads(ws.recv())
+    producer.produce(json.dumps(result).encode("utf-8"))
+
+
