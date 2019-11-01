8b1860709dff93cf89804d8bb04639a15700d38c
srivatsanrp <40450617+srivatsanrp@users.noreply.github.com>
2019-11-01 20:48:17 +0530

    Update second.py

--- second.py
+++ second.py
@@ -7,9 +7,9 @@ from pykafka.exceptions import SocketDisconnectedError
 from apscheduler.schedulers.background import BackgroundScheduler
 
 def count_txn_rate():
-    global r, cnt
-    temp = cnt
-    cnt = 0
+    global r, count
+    temp = count
+    count = 0
     pref = "r"
     r.set(pref + str(datetime.datetime.now().strftime("%H:%M")), str(temp), ex=3600)
 
@@ -21,13 +21,13 @@ consumer = topic.get_simple_consumer(
     reset_offset_on_start=True
 )
 scheduleRate = BackgroundScheduler()
-cnt = 0
+count = 0
 scheduleRate.add_job(count_txn_rate, 'cron', second='0')
 scheduleRate.start()
 pre = "v"
 for message in consumer:
     if message is not None:
-        cnt += 1
+        count += 1
         x = message.value.decode("utf-8")
         jsonVar = json.loads(x)
         r.lpush("transactions", json.dumps(jsonVar).encode("utf-8"))
