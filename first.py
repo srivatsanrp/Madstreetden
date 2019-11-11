905648137c7e41666dd721b4a4f69377ae792e28
srivatsanrp <40450617+srivatsanrp@users.noreply.github.com>
2019-11-11 12:39:40 +0530

    Update first.py

--- first.py
+++ first.py
@@ -8,7 +8,7 @@ producer = topic.get_producer()
 topic.get_sync_producer() 
 ws = create_connection("wss://ws.blockchain.info/inv")
 ws.send(json.dumps({"op": "unconfirmed_sub"}))
-while True:
+
     result = json.loads(ws.recv())
     producer.produce(json.dumps(result).encode("utf-8"))
 
